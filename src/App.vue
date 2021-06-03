f2a82337fee0303e886955c8b27068f2fb740410
chenmeiqi <micky267@163.com>
2021-06-03 15:00:32 +0800

    0.1.6-2

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
  * @Autor: chenmeiqi
  * @LastEditors: chenmeiqi
  * @Date: 2019-12-03 11:03:37
- * @LastEditTime: 2021-06-03 14:48:25
+ * @LastEditTime: 2021-06-03 14:59:17
 -->
 <template>
   <div id="app">
     <router-view/>
-    master
+    master-2.0
   </div>
 </template>
 
