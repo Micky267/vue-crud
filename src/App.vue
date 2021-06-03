740239efe5e5578fa2c5a1993c39a5642eec83c7
chenmeiqi <micky267@163.com>
2021-06-03 14:48:46 +0800

    5

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
+<!--
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: chenmeiqi
+ * @LastEditors: chenmeiqi
+ * @Date: 2019-12-03 11:03:37
+ * @LastEditTime: 2021-06-03 14:48:25
+-->
 <template>
   <div id="app">
     <router-view/>
+    master
   </div>
 </template>
 
